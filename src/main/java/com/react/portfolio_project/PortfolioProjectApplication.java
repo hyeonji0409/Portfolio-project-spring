677ab8503eff8199ebe84087f2e3d488c1822251
yuyusss <gch04713@naver.com>
2022-03-18 15:22:13 +0900

    u

--- src/main/java/com/react/portfolio_project/PortfolioProjectApplication.java
+++ src/main/java/com/react/portfolio_project/PortfolioProjectApplication.java
@@ -1,4 +1,4 @@
-package com.react.portfolio_project;
+package com.react.portfolio_project; 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
