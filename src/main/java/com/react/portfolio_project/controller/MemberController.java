2fcb823f0c72cef21ca8c4468e31560dedba6cab
yuyusss <gch04713@naver.com>
2022-03-21 19:15:10 +0900

    3/21 yu

--- src/main/java/com/react/portfolio_project/controller/MemberController.java
+++ src/main/java/com/react/portfolio_project/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.react.portfolio_project.controller;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.react.portfolio_project.model.MemberVO;
@@ -19,5 +25,59 @@ public class MemberController {
 	@RequestMapping(value="/signup")
 	public void signUp(MemberVO vo) {		
 		service.signUp(vo);	 
+	}
+	
+	@ResponseBody // 바디객체에 호출
+	@RequestMapping("/login")
+	public String login(MemberVO vo,
+	//public String login(@RequestParam HashMap<String, Object> param,
+						HttpSession session) {
+		
+		HashMap<String, Object> param = new HashMap<String, Object>();
+		param.put("memId", vo.getMemId());
+		param.put("memPw", vo.getMemPw());
+		
+//		System.out.println(param.get("memId"));
+//		System.out.println(param.get("memPw"));
+		
+		// DB에서 아이디 패스워드 확인
+		MemberVO resultMember = service.login(param);
+		//System.out.println(resultMember.getMemId());
+		String result = "fail";
+				
+		if(resultMember != null) {
+			session.setAttribute("sid1", resultMember.getMemId());
+//			session.setAttribute("sid", resultMember);
+			 result = "ok";			 
+		System.out.println("성공");
+		}
+		//System.out.println(resultMember.getMemId());
+		return result;
+	}
+	
+	@RequestMapping("/logout")
+	public void logout(HttpSession session) {
+		
+		// DB에서 아이디 패스워드 확인
+
+		session.invalidate();
+
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/idcheck")
+	public String idCheck(@RequestParam String id) {
+
+		System.out.println(id);
+
+		String checkVar = "no_use";
+
+		String memId_result = service.memIdCheck(id);
+		
+		if(memId_result != null) {
+
+			checkVar = "use";
+		}
+		return checkVar;
 	}
 }
